8ff6cdb063b4c0eaaff65eab46584f4d2f2b4aeb
Daniel Meilleur <daniel.meilleur@gmail.com>
2019-05-29 01:45:08 -0700

    Setup dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,7 @@ module.exports = (env) => {
     mode: isProduction ? 'production' : 'development',
     // mode: 'development',
     output: {
-      path: path.join(__dirname, 'public'),
+      path: path.join(__dirname, 'public', 'dist'),
       filename: 'bundle.js'
     },
     module: {
@@ -77,7 +77,8 @@ module.exports = (env) => {
     devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
-      historyApiFallback: true
+      historyApiFallback: true,
+      publicPath: '/dist/'
     }
   }
 }
