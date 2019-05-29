2722a4ed4ed5a29c500b6c432f2b8275be6ec9e1
Daniel Meilleur <daniel.meilleur@gmail.com>
2019-05-28 23:52:10 -0700

    L136 Setup production build and server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 // server.js
+/* global process */
 const path = require('path')
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 3000
 
 // eslint-disable-next-line no-undef
 const publicPath = path.join(__dirname, '..', 'public')
@@ -10,6 +12,6 @@ app.use(express.static(publicPath))
 app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'))
 })
-app.listen(3000, () => {
-  console.log('server is up on port 3000')
+app.listen(port, () => {
+  console.log(`server is up on port ${port}`)
 })
